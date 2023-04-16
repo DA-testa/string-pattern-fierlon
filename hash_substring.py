bc7f1c73dd86dc7b53b13366ad27b2a3563b9fa4
Kirils Baranovs <125561803+fierlon@users.noreply.github.com>
2023-04-16 20:55:43 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,34 @@
 # python3
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    input_text = input().rstrip()
+    if input_text == 'i':
+        pattern = input().rstrip()
+        text = input().rstrip()
+    elif input_text == 'f':
+        with open('input.txt', 'r') as file:
+            pattern = file.readline().rstrip()
+            text = file.readline().rstrip()
+    return pattern, text
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    prime = 101
+    p_len = len(pattern)
+    t_len = len(text)
+    p_hash = sum(ord(c) for c in pattern)
+    t_hash = sum(ord(c) for c in text[:p_len])
+    occurrences = []
 
-    # and return an iterable variable
-    return [0]
+    for i in range(t_len - p_len + 1):
+        if p_hash == t_hash and pattern == text[i:i+p_len]:
+            occurrences.append(i)
+        if i < t_len - p_len:
+            t_hash = t_hash - ord(text[i]) + ord(text[i+p_len])
+    return occurrences
 
-
-# this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
 
